aec7847fa69b0e221d8986d925f63f05817bd48e
charanch2 <chandi123mailme@gmail.com>
2018-07-30 14:38:56 +0530

    Sample file commit

---
+++ testproject/src/main/java/chandi/testproject/Sample.java
@@ -0,0 +1,10 @@
+package chandi.testproject;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.print("Sample code for git testing");
+	}
+
+}

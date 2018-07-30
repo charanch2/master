292c95b54ad0a83e978268f6ff77a595b16914d1
charanch2 <chandi123mailme@gmail.com>
2018-07-30 14:48:34 +0530

    commit to check about version

--- testproject/src/main/java/chandi/testproject/App.java
+++ testproject/src/main/java/chandi/testproject/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.print("test to check history of files for comparison");
     }
 }
